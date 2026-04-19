9cdc61d34f5c284c36097248b417b520b75cb13a
growziq <dev.growziq@gmail.com>
2026-04-20 00:36:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    white: '#FFFFFF',
+                    accent: '#C7D2FE',
+                    action: '#8B5CF6',
+                    actionHover: '#7C3AED',
+                    cyan: '#60A5FA',
+                    dark: '#1F2937',
+                    darkHover: '#111827'
+                }
+            },
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+            },
+            maxWidth: {
+                'container': '1120px',
+            },
+            borderRadius: {
+                'btn': '10px',
+            }
+        }
+    }
+};
